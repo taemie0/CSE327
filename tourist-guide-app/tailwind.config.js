c8cba7507bd648d5a37666b1437106b8c42cd3a7
Meharun Nesa <nesameharun345@gmail.com>
2024-11-26 22:28:49 +0600

    Initial React Native setup

---
+++ tourist-guide-app/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.js", // Include the main app entry point
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all files in the `src` folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
