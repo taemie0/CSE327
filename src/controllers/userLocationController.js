94eb72bac89a70139f5b8d2abc35033b73f297f4
umme17 <ummehabiba17805hk@gmail.com>
2024-12-01 20:06:04 +0600

    2nd commint

--- src/controllers/userLocationController.js
+++ src/controllers/userLocationController.js
@@ -1,6 +1,12 @@
 // src/controllers/userLocationController.js
 import { getCurrentLocation } from '../models/userLocationModel';  // Import the Model
 
+/**
+ * Fetches the user's current location using the `getCurrentLocation` model function.
+ * @async
+ * @function fetchUserLocation
+ * @returns {Object|null} The user's location object (latitude and longitude), or null if the location cannot be fetched.
+ */
 export async function fetchUserLocation() {
   const location = await getCurrentLocation();  // Get location from Model
   if (location) {
