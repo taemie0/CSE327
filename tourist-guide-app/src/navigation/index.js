6b3dfdda917b405977888b86991a8c808bddcc1d
ramizarahman <ramizarb25@gmail.com>
2024-12-02 03:00:25 +0600

    Update index.js

---
+++ tourist-guide-app/src/navigation/index.js
@@ -0,0 +1,5 @@
+import { registerRootComponent } from 'expo';
+import App from './App'; // Import your main App component
+
+// Register the main App component with Expo
+registerRootComponent(App);
