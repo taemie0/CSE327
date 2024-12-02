d65cf754eee33543f3e44c6ab05148f0dcac6c01
ramizarahman <ramizarb25@gmail.com>
2024-12-02 12:35:21 +0600

    Update index.js

---
+++ tourist-guide-app/src/utils/index.js
@@ -0,0 +1,5 @@
+// Export functions from locationUtils.js
+import { getCurrentLocation, geocodeLocation } from './locationUtils';
+
+// Bundle them together to export
+export { getCurrentLocation, geocodeLocation };
